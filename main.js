70a01d0499f11c2be1058020bdbd95194648503e
Paulina <p.wisniewska285@gmail.com>
2021-09-13 17:17:17 +0200

    Create README.md

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const spanTxt = document.getElementById("text");
-const aboutMe = "           Pochodzę z Torunia i jestem absolwentką Politechniki Gdańskiej. Poszukuję stażu lub praktyk związanych z Front-Endem. Obecnie pracuję jako Personalized Internet Assessor w przedsiębiorstwie wspomagającym rozwój sztucznej inteligencji. Uwielbiam podróżować oraz interesuję się fotografią i ogrodnictwem.";
+const aboutMe = "and I currently work as ServiceNow Developer, but I would like to expand other technologies connected with Front-End. I am a graduate of Gdansk University of Technology and I have completed postgraduate studies - Web Development. I would like to improve my previously acquired skills.";
 
 
 let index = 0;
-const time = 80;
+const time = 60;
 
 const addLetter = () => {
     spanTxt.textContent += aboutMe[index];
@@ -16,7 +16,7 @@ const letterTyping = setInterval(addLetter, time);
 
 AOS.init({
     delay: 150,
-    duration: 3000,
+    duration: 10000,
     once: false,
 })
 
